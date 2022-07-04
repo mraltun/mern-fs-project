9c1fc6dc4823a7f51c4180235166d0ccc77e166e
Murat Altun <mraltun@gmail.com>
2022-07-04 17:43:43 +0300

    Setting up folders and added Users page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import Users from "./user/pages/Users";
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Route path='/' exact>
+        <Users />
+      </Route>
+      <Redirect to='/' />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
