da105fa9bb1c05af0a9f1c497c472356f294c2b2
Murat Altun <mraltun@gmail.com>
2022-07-04 17:55:17 +0300

    Added NewPlace and routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Route path='/' exact>
-        <Users />
-      </Route>
-      <Redirect to='/' />
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+        <Route path='/places/new'>
+          <NewPlace />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
     </BrowserRouter>
   );
 };
