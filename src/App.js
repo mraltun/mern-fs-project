3e6b487cc1726374d815f435e3518fbf087864c7
Murat Altun <mraltun@gmail.com>
2022-07-05 14:37:13 +0300

    PlaceItem and UserPlaces components done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 
 const App = () => {
   return (
@@ -11,6 +12,9 @@ const App = () => {
         <Switch>
           <Route path='/' exact>
             <Users />
+          </Route>
+          <Route path='/:userId/places' exact>
+            <UserPlaces />
           </Route>
           <Route path='/places/new'>
             <NewPlace />
