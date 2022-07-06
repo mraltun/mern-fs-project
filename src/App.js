c2e596be2f7bfc99da334607b804ca6eb7d31789
Murat Altun <mraltun@gmail.com>
2022-07-06 13:08:39 +0300

    Added UpdatePlace component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
 
 const App = () => {
   return (
@@ -18,6 +19,9 @@ const App = () => {
           </Route>
           <Route path='/places/new'>
             <NewPlace />
+          </Route>
+          <Route path='/places/:placeId' exact>
+            <UpdatePlace />
           </Route>
           <Redirect to='/' />
         </Switch>
--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -32,6 +32,11 @@ const formReducer = (state, action) => {
 };
 
 const NewPlace = () => {
+  const placeSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(formState.inputs);
+  };
+
   const [formState, dispatch] = useReducer(formReducer, {
     inputs: {
       title: {
@@ -58,7 +63,7 @@ const NewPlace = () => {
   }, []);
 
   return (
-    <form className='place-form'>
+    <form className='place-form' onSubmit={placeSubmitHandler}>
       <Input
         id='title'
         element='input'
@@ -74,6 +79,14 @@ const NewPlace = () => {
         label='Description'
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText='Please enter a valid description (at least 5 characters)'
+        onInput={inputHandler}
+      />
+      <Input
+        id='address'
+        element='input'
+        label='Address'
+        validators={[VALIDATOR_REQUIRE]}
+        errorText='Please enter a valid address'
         onInput={inputHandler}
       />
       <Button type='submit' disabled={!formState.isValid}>
